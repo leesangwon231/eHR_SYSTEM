f60cffbd26fee20d88a09cc8f9e38da1985b63b6
Sangwon Lee <werggf@naver.com>
2022-07-22 11:11:03 +0900

    저널 DTO 추가

---
+++ eHR_SYSTEM/src/main/java/com/mis/dto/JournalDTO.java
@@ -0,0 +1,12 @@
+package com.mis.dto;
+
+public class JournalDTO {
+	private int memNo;
+	private String memName;
+	private String memEmail;
+	private String memZipcode;
+	private String memAddr1;
+	private String memAddr2;
+}
+
+

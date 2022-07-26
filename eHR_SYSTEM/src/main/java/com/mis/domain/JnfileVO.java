2c865b75ab6d6ea3f01f70191334bda8bfec4688
user <user@DESKTOP-H626ASH>
2022-07-26 11:14:51 +0900

    dddd

--- eHR_SYSTEM/src/main/java/com/mis/domain/JnfileVO.java
+++ eHR_SYSTEM/src/main/java/com/mis/domain/JnfileVO.java
@@ -5,29 +5,44 @@ public class JnfileVO {
 	private int jnfileNo;
 	private String jnfileName;
 	private int jndNo;
+	private String fileLocation;
+
 	public int getJnfileNo() {
 		return jnfileNo;
 	}
+
 	public void setJnfileNo(int jnfileNo) {
 		this.jnfileNo = jnfileNo;
 	}
+
 	public String getJnfileName() {
 		return jnfileName;
 	}
+
 	public void setJnfileName(String jnfileName) {
 		this.jnfileName = jnfileName;
 	}
+
 	public int getJndNo() {
 		return jndNo;
 	}
+
 	public void setJndNo(int jndNo) {
 		this.jndNo = jndNo;
 	}
+
+	public String getFileLocation() {
+		return fileLocation;
+	}
+
+	public void setFileLocation(String fileLocation) {
+		this.fileLocation = fileLocation;
+	}
+
 	@Override
 	public String toString() {
-		return "JnfileVO [jnfileNo=" + jnfileNo + ", jnfileName=" + jnfileName + ", jndNo=" + jndNo + "]";
+		return "JnfileVO [jnfileNo=" + jnfileNo + ", jnfileName=" + jnfileName + ", jndNo=" + jndNo + ", fileLocation="
+				+ fileLocation + "]";
 	}
-	
-	
-	
+
 }
